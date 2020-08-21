eb21be7347c751bb3d04684b34afcbc671ba91ee
Jason Gayle <jasonblue1626@gmail.com>
2020-08-21 01:09:00 -0500

    Venv installation with documentation

--- setup/_pattoo_agent_bacnet/configure.py
+++ setup/_pattoo_agent_bacnet/configure.py
@@ -1,10 +1,11 @@
 """Configure the bacnet agent."""
 import os
+from _pattoo_agent_bacnet import shared as _shared
 from pattoo_shared.installation import configure, shared
 from pattoo_shared import files
 
 
-def install():
+def install(pattoo_home):
     """Start configuration process.
 
     Args:
@@ -17,24 +18,27 @@ def install():
     # Initialize key variables
     if os.environ.get('PATTOO_CONFIGDIR') is None:
         os.environ['PATTOO_CONFIGDIR'] = '{0}etc{0}pattoo'.format(os.sep)
-    config_directory = os.environ.get('PATTOO_CONFIGDIR')
+    config_dir = os.environ.get('PATTOO_CONFIGDIR')
 
     bacnet_agent_dict = {
             'polling_interval': 300,
     }
 
     # Attempt to create configuration directory
-    files.mkdir(config_directory)
+    files.mkdir(config_dir)
 
     # Create the pattoo user and group
-    configure.create_user('pattoo', '/nonexistent', ' /bin/false', True)
+    if _shared.root_check() is True:
+        # Create the pattoo user and group
+        configure.create_user('pattoo', pattoo_home, '/bin/false', True)
 
-    # Attempt to change the ownership of the configuration directory
-    shared.chown(config_directory)
+        # Attempt to change the ownership of the config and home directories
+        shared.chown(config_dir)
+        shared.chown(pattoo_home)
 
     config_file = configure.pattoo_config(
                                         'pattoo_agent_bacnetipd',
-                                        config_directory,
+                                        config_dir,
                                         bacnet_agent_dict)
 
     configure.check_config(config_file, bacnet_agent_dict)
